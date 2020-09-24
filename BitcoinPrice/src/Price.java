9f3ad8486ba4e2ac3ab70094008ac1e12c0fc52a
DanielHoulihan <49064725+DanielHoulihan@users.noreply.github.com>
2020-09-24 19:05:40 +0100

    Add files via upload

--- BitcoinPrice/src/Price.java
+++ BitcoinPrice/src/Price.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class Price {
 
@@ -34,47 +36,72 @@ public class Price {
                 reader.close();
             }
             System.out.println(response.toString());
+            ObjectMapper o = new ObjectMapper();
+            String json = response.toString();
+            Data data = o.readValue(json, Data.class);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
+// date must be in format yyyy-mm-dd
+    public void getPriceAtDate(String startDate, String endDate) {
 
-    public void getPriceAtDate(String date) {
-
-//        BufferedReader reader;
-//        String line;
-//        StringBuffer responseContent = new StringBuffer();
-//        try {
-//            URL url = new URL("https://api.coindesk.com/v1/bpi/currentprice.json");
-//            connection = (HttpURLConnection) url.openConnection();
-//            connection.setRequestMethod("GET");
-//            connection.setConnectTimeout(10000);
-//            connection.setReadTimeout(10000);
-//            int status = connection.getResponseCode();
-////            System.out.println(status);
-//            if (status > 299) {
-//                reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-//                while ((line = reader.readLine()) != null) {
-//                    responseContent.append(line);
-//                }
-//                reader.close();
-//            } else {
-//                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//                while ((line = reader.readLine()) != null) {
-//                    responseContent.append(line);
-//                }
-//                reader.close();
-//            }
-//            System.out.println(responseContent.toString());
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        BufferedReader reader;
+        String line;
+        StringBuffer responseContent = new StringBuffer();
+        try {
+            URL url = new URL("https://api.coindesk.com/v1/bpi/historical/close.json?start="+startDate+"&end="+endDate);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(10000);
+            connection.setReadTimeout(10000);
+            int status = connection.getResponseCode();
+//            System.out.println(status);
+            if (status > 299) {
+                reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+                while ((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                reader.close();
+            } else {
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                while ((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                reader.close();
+            }
+            System.out.println(responseContent.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
+    class JsonString{
+        Data data;
+    }
+
+    class Data {
+        String base;
+        String currency;
+        Double amount;
+    }
+
+
+    public double bitcoinAtTimeOfInvestment(double priceOfBitcoin, double investment){
+        priceOfBitcoin=0;
+        investment=0;
+        return priceOfBitcoin/investment;
+
+    }
+
+    public double currentPriceOfInvestment(double bitcoinOwned, double currentPrice){
+        return bitcoinOwned * currentPrice;
+    }
+
+
 
 
 }
